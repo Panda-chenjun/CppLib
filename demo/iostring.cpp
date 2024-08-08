f7d78d3d24741486680d769f074e115455fa0b9c
panda-chejun <fendou_panda@qq.com>
2024-08-08 17:15:17 +0800

    update

--- demo/iostring.cpp
+++ demo/iostring.cpp
@@ -1,6 +1,19 @@
 #include"../any.h"
 #include<bits/stdc++.h>
+#define f(Tp,str) if(os==str){Tp x;Cin>>x;Cout<<x<<"\n";}
 using namespace std;
 int main(){
-	Cout<<"Welcome to CppLib testing"
+	Cout<<"Welcome to CppLib testing!\n";
+	Cout<<"Tips:I=int,F=float,D=double,L=long long,C=char,S=string\n";
+	Cout<<"press Ctrl+C to exit\n";
+	while(1){
+		string os;
+		Cin>>os;
+		f(int,"I");
+		f(float,"F");
+		f(double,"D");
+		f(long long,"L");
+		f(char,"C");
+		f(string,"S");
+	}
 }
