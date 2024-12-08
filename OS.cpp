74c6fdbc105d035c3ec99e677e6be5af4c348b6f
panda-chejun <fendou_panda@qq.com>
2024-12-08 11:34:43 +0800

    update

--- OS.cpp
+++ OS.cpp
@@ -13,19 +13,34 @@ class OS{
 	int (*fn)(const char*,...) = printf;
 	public:
 	auto& operator<<(const int& x){fn("%d",x);return*this;}
+	auto& operator<<(const int&& x){fn("%d",x);return*this;}
 	auto& operator<<(const char& x){fn("%c",x);return*this;}
+	auto& operator<<(const char&& x){fn("%c",x);return*this;}
 	auto& operator<<(const float& x){fn("%f",x);return*this;}
+	auto& operator<<(const float&& x){fn("%f",x);return*this;}
 	auto& operator<<(const double& x){fn("%lf",x);return*this;}
+	auto& operator<<(const double&& x){fn("%lf",x);return*this;}
 	auto& operator<<(const long long& x){fn("%lld",x);return*this;}
+	auto& operator<<(const long long&& x){fn("%lld",x);return*this;}
 	auto& operator<<(const char* x){fn("%s",x);return*this;}
 	OS& operator<<(const short& x){fn("%d",x);return*this;}
-	OS& operator<<(std::string str){
+	OS& operator<<(const short&& x){fn("%d",x);return*this;}
+	OS& operator<<(const std::string& str){
+		return *this<<str.data();
+	}
+	OS& operator<<(const std::string&& str){
 		return *this<<str.data();
 	}
 	template<const unsigned long long N> 
-	OS& operator<<(std::bitset<N> t){
+	OS& operator<<(const std::bitset<N>& t){
 		for(int i=0;i<N;i++)
 			fn("%d",t[i]);
 	}
+	OS& operator<<(const __int128& x){
+		if(x>10)
+			*this<<(x/10);
+		return *this<<int(x%10);
+	}
 }Cout;
+
 END
