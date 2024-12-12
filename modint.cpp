d301f393bcb223c111e9ca2c504379280672f9f4
panda-chejun <fendou_panda@qq.com>
2024-12-12 18:26:47 +0800

    update

--- modint.cpp
+++ modint.cpp
@@ -1,11 +1,25 @@
 #include"IOS.cpp"
 #include"cxxtype.cpp"
-template<unsigned long long P>
+template<ull P>
 class modint{
 	private:
-	
+	ll v;
+	public:
+	modint(ll x){v=x%P;}
+	modint(){v=0;}
+	fll val(){
+		return this->v;
+	}
+	bool operator<(modint __rhs){return v<__rhs.v;}
+	bool operator>(modint __rhs){return v>__rhs.v;}
+	bool operator<=(modint __rhs){return v<=__rhs.v;}
+	bool operator>=(modint __rhs){return v>=__rhs.v;}
+	modint operator+(modint __rhs){return (v+__rhs.v)%P;}
+	modint operator-(modint __rhs){return (v-__rhs.v+P)%P;}
+	modint operator*(modint __rhs){return (v*__rhs.v)%P;}
+	modint operator/(modint __rhs){return (v/__rhs.v)%P;}
 };
-template<unsigned long long P>
-bool operator << (std::OS x,modint<P> A){
-	
+template<ull P>
+std::OS& operator << (std::OS x,modint<P> A){
+	return x<<A.val();
 }
