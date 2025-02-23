456e0caf2d31aa5e5a7dc5893e236aa1e7ffdcf8
panda-chejun <fendou_panda@qq.com>
2025-02-23 10:42:18 +0800

    update.

--- src/stdio.h
+++ src/stdio.h
@@ -1,22 +1,29 @@
-// GNU C++ Lib getfunc.h
-// Author: Panda-chenjun
-// This file is part of the GNU C++ Lib.
+// <cmath> -*- C++ -*-
+
+// Copyright (C) 2001-2023 Free Software Foundation, Inc.
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, see <http://www.gnu.org/licenses/>.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
-#ifndef __GNU_CXX_GETFUNC_H
-#define __GNU_CXX_GETFUNC_H
+// Under Section 7 of GPL version 3, you are granted additional
+// permissions described in the GCC Runtime Library Exception, version
+// 3.1, as published by the Free Software Foundation.
+
+// You should have received a copy of the GNU General Public License and
+// a copy of the GCC Runtime Library Exception along with this program;
+// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#ifndef __EX_STDIO_H
+#define __EX_STDIO_H
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -24,4 +31,4 @@
 
 
 
-#endif // __GNU_CXX_GETFUNC_H
+#endif // __EX_STDIO_H
